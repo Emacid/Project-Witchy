46f47fc0dd6bf43d579869daf18aed4a9c22594e
Zerda Ece UYĞUN <zerdaeceuygun@posta.mu.edu.tr>
2024-03-09 18:58:52 +0300

    2D lerin oyuncuya bakması
    .

--- Assets/Ece/Scripts/Billboarding.cs
+++ Assets/Ece/Scripts/Billboarding.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Billboarding : MonoBehaviour
 {
+    private Camera theCam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        theCam = Camera.main; 
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.LookAt(theCam.transform);
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y,0f);
     }
+
 }
